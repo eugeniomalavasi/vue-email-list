6c9b0cd6932e3697e93df155901a3d221563b386
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-29 21:12:21 +0200

    feat: loading text

--- js/script.js
+++ js/script.js
@@ -4,17 +4,25 @@ createApp({
     data() {
         return {
             emailArray: [],
-            emailTotal: 0
+            emailTotal: 0,
+            loading: null
         }
     }, methods: {
         emailGen: function (emailTotal) {
-            this.emailArray = []; 
-            for(let i = 0; i < emailTotal; i++) {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email) => {
-                this.emailArray.push(email.data.response)
-                console.log(this.emailArray);
-                console.log(this.emailArray.length);
-            })            
-        }}
+            this.emailArray = [];
+            for (let i = 0; i < emailTotal; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email) => {
+                    this.emailArray.push(email.data.response)
+                    console.log(this.emailArray);
+                    console.log(this.emailArray.length);
+                    this.loading = true;
+                    if (this.emailArray.length == this.emailTotal) {
+                        this.loading = false;
+                    } 
+                    console.log(this.loading);
+                })
+            }}, loadingText: function () {
+                
+        }
     },
 }).mount('#app')
