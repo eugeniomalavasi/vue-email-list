1666d1167f0e3adda6366588ce47c63a9f45a7b3
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-29 15:34:30 +0200

    feat: now is possible to choose how many email generate

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ createApp({
     data() {
         return {
             emailArray: [],
+            emailTotal: 0
         }
     }, methods: {
-        emailGen: function () {
-            for(let i = 0; i < 10; i++) {
+        emailGen: function (emailTotal) {
+            for(let i = 0; i < emailTotal; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email) => {
                 this.emailArray.push(email.data.response)
                 console.log(this.emailArray);
             })
+            console.log(this.emailTotal);
         }}
     },
 }).mount('#app')
