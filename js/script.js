f12d8e5a6603a2214dbe4f0de96741c1f27fb9ac
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-29 21:14:11 +0200

    fix: the order of the loading flag is now the first thing in the then

--- js/script.js
+++ js/script.js
@@ -12,17 +12,16 @@ createApp({
             this.emailArray = [];
             for (let i = 0; i < emailTotal; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((email) => {
+                    this.loading = true;
                     this.emailArray.push(email.data.response)
                     console.log(this.emailArray);
                     console.log(this.emailArray.length);
-                    this.loading = true;
                     if (this.emailArray.length == this.emailTotal) {
                         this.loading = false;
-                    } 
+                    }
                     console.log(this.loading);
                 })
-            }}, loadingText: function () {
-                
-        }
+            }
+        },
     },
 }).mount('#app')
